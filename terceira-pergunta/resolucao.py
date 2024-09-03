580c1e73914e854511fef00a0e59a15062976d81
gusilvo <gustavossilva13@gmail.com>
2024-09-03 17:48:12 -0300

    atualização dos dados

--- terceira-pergunta/resolucao.py
+++ terceira-pergunta/resolucao.py
@@ -3,13 +3,13 @@ import json
 def menor_valor_de_faturamento(dados):
     menor_numero = None
 
-    for v in dados.values():
-        if v == None:
+    for d in dados:
+        if d['valor'] == 0:
             pass
         elif menor_numero == None:
-            menor_numero = v
-        elif v < menor_numero:
-            menor_numero = v
+            menor_numero = d['valor']
+        elif d['valor'] < menor_numero:
+            menor_numero = d['valor']
 
     return menor_numero
 
@@ -17,13 +17,13 @@ def menor_valor_de_faturamento(dados):
 def maior_valor_de_faturamento(dados):
     maior_numero = None
 
-    for v in dados.values():
-        if v == None:
+    for d in dados:
+        if d['valor'] == 0:
             pass
         elif maior_numero == None:
-            maior_numero = v
-        elif v > maior_numero:
-            maior_numero = v
+            maior_numero = d['valor']
+        elif d['valor'] > maior_numero:
+            maior_numero = d['valor']
 
     return maior_numero
 
@@ -31,28 +31,28 @@ def maior_valor_de_faturamento(dados):
 def dias_com_valores_superiores(dados):
     total = dias = dias_superior = 0
 
-    for v in dados.values():
-        if v != None:
-            total += v
+    for d in dados:
+        if d['valor'] != 0:
+            total += d['valor']
             dias += 1
     
     media = total / dias
 
-    for n in dados.values():
-        if n == None:
+    for n in dados:
+        if n['valor'] == 0:
             pass
-        elif n > media:
+        elif n['valor'] > media:
             dias_superior += 1
 
     return dias_superior
 
 
 # Programa Principal
-with open('vetor.json', 'r') as file:
+with open('dados.json', 'r') as file:
     dados_faturamento = json.load(file)
 
 print(
-    f'O menor valor de faturamento ocorrido em um dia do mês: {menor_valor_de_faturamento(dados_faturamento["daily_revenue"])}\n'
-    f'O maior valor de faturamento ocorrido em um dia do mês foi: {maior_valor_de_faturamento(dados_faturamento["daily_revenue"])}\n'
-    f'O número de dias no mês em que o valor de faturamento diário foi superior à média mensal foi: {dias_com_valores_superiores(dados_faturamento["daily_revenue"])}'
+    f'O menor valor de faturamento ocorrido em um dia do mês foi {menor_valor_de_faturamento(dados_faturamento)}\n'
+    f'O maior valor de faturamento ocorrido em um dia do mês foi {maior_valor_de_faturamento(dados_faturamento)}\n'
+    f'O número de dias no mês em que o valor de faturamento diário foi superior à média mensal foi de {dias_com_valores_superiores(dados_faturamento)}'
 )
